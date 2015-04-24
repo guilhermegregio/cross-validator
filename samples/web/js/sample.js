0ef8db4cec12f081fd8f919f2fc2fedd09728ea1
Guilherme Mangabeira Gregio <guilherme@3gweb.com.br>
2015-04-24 13:47:30 -0300

    change sample

--- samples/web/js/sample.js
+++ samples/web/js/sample.js
@@ -1,3 +1,38 @@
 (function () {
-	console.log(new CrossValidator({name: ''}, ['notEmpty($name)']).validate().hasErrors());
+	console.log('=========');
+	console.log('VALIDOS');
+	console.log('=========');
+	console.log(CrossValidator.forData({name: 'guilherme'}).using(['notNull($name)']).isValid());
+	console.log(CrossValidator.forData({name: 'guilherme'}).using(['notEmpty($name)']).isValid());
+	console.log(CrossValidator.forData({name: '1'}).using(['isNumber($name)']).isValid());
+	console.log(CrossValidator.forData({name: 'guilherme@gregio.net'}).using(['isEmail($name)']).isValid());
+	console.log(CrossValidator.forData({name: 'guilherme'}).using(['isEita($name)']).isValid());
+	console.log('=========');
+	console.log('INVALIDOS');
+	console.log('=========');
+	console.log(CrossValidator.forData({name: null}).using(['notNull($name)']).isValid());
+	console.log(CrossValidator.forData({name: ''}).using(['notEmpty($name)']).isValid());
+	console.log(CrossValidator.forData({name: 'a1'}).using(['isNumber($name)']).isValid());
+	console.log(CrossValidator.forData({name: 'guilhermegregio.net'}).using(['isEmail($name)']).isValid());
+	console.log(CrossValidator.forData({name: 'guilherme'}).using(['isEita($name)']).isValid());
+	console.log('=========');
+	console.log('LOOP');
+	console.log('=========');
+	var start = Date.now();
+	for (var i = 10000; i--;) {
+		CrossValidator.forData({name: 'guilherme'}).using(['notNull($name)']).isValid();
+		CrossValidator.forData({name: 'guilherme'}).using(['notEmpty($name)']).isValid();
+		CrossValidator.forData({name: '1'}).using(['isNumber($name)']).isValid();
+		CrossValidator.forData({name: 'guilherme@gregio.net'}).using(['isEmail($name)']).isValid();
+		CrossValidator.forData({name: 'guilherme'}).using(['isEita($name)']).isValid();
+
+		CrossValidator.forData({name: null}).using(['notNull($name)']).isValid();
+		CrossValidator.forData({name: ''}).using(['notEmpty($name)']).isValid();
+		CrossValidator.forData({name: 'a1'}).using(['isNumber($name)']).isValid();
+		CrossValidator.forData({name: 'guilhermegregio.net'}).using(['isEmail($name)']).isValid();
+		CrossValidator.forData({name: 'guilherme'}).using(['isEita($name)']).isValid();
+	}
+	var end = Date.now();
+	console.log('Time: ', end - start);
+	console.log('=========');
 })();
